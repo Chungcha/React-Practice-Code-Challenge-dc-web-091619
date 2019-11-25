381f4429c2df25d2895d8e474d44949e8404e37a
chungcha <charlieschung@gmail.com>
2019-11-24 23:02:50 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -3,15 +3,90 @@ import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis/?_limit=4"
 
-class App extends Component {
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      shownSushi: [],
+      wallet: "100",
+      page: 1,
+      plates: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(`${API}&_page=${this.state.page}`)
+    .then(response=>response.json())
+    .then(data=>{
+      this.setState({
+        shownSushi: [...data]
+      })
+    })
+  }
+
+  renderMoreSushi = () => {
+    this.setState({
+      page: ++this.state.page
+    })
+    fetch(`${API}&_page=${this.state.page}`)
+    .then(response=>response.json())
+    .then(data=>{
+      let newSushi = [...data]
+      this.setState({
+        shownSushi: newSushi
+      })
+    })
+  }
+
+  // eatSushi = (id) => {
+  //   let updatedList = this.state.shownSushi.map(sushi=>{
+  //     if (sushi.id === id){
+  //       let newBudget = this.state.wallet - sushi.price
+  //       if (newBudget < 0) {
+  //         alert("Cant Eat this food.")
+  //         return {...sushi, eaten: false}
+  //       } else {
+  //       this.setState({
+  //         wallet: newBudget
+  //       })}
+  //       return {...sushi, eaten: true}
+  //     } else {
+  //       return sushi
+  //     }
+  //   })
+  //   this.setState({
+  //     shownSushi: updatedList
+  //   })
+  // }
+
+    eatSushi = (id) => {
+      let sushi = this.state.shownSushi.find(sushi=>sushi.id ===id)
+      let newBudget = this.state.wallet - sushi.price
+      if (newBudget < 0 || sushi.eaten === true){
+        alert ("NO CAN DO")
+      } else {
+        let updatedList = this.state.shownSushi.map(sushi=>{
+          if (sushi.id === id) {
+            return {...sushi, eaten: true}
+          } else {
+            return sushi
+          }
+        })
+        this.setState({
+          wallet: newBudget,
+          shownSushi: updatedList,
+          plates: [...this.state.plates, id]
+      })
+      }
+    }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer shownSushi={this.state.shownSushi} renderMoreSushi={this.renderMoreSushi} eatSushi={this.eatSushi}/>
+        <Table wallet={this.state.wallet} plates={this.state.plates}/>
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,20 +1,19 @@
 import React, { Fragment } from 'react'
+import picture1 from "../assets/RJGr3Xs.png"
+import picture2 from "../assets/sushi-slice_29.png"
+import picture3 from "../assets/sushi-slice_75.png"
+import picture4 from "../assets/sushi-slice_99.png"
 
 const Sushi = (props) => {
+  let num = Math.floor(Math.random()*4)+1
   return (
     <div className="sushi">
       <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          true ?
-            null
-          :
-            <img src={/* Give me an image source! */} width="100%" />
-        }
+           onClick={()=>props.eatSushi(props.sushi.id)}>
+        {props.sushi.eaten === true ? null : <img src={picture1} width="100%" />}
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {props.sushi.name} - ${props.sushi.price}
       </h4>
     </div>
   )
